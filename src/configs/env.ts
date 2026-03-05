f5ef8f79f928e70e46ce99065e1f6ff7043f151a
LucaFrioli <lucafrioli7@gmail.com>
2026-03-05 13:50:57 -0300

    chore: add Provider de NanoId

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -9,6 +9,7 @@ const enabledDatabaseConections = ['mongodb'] as const;
 export const supportedHashProviders = ['argon2', 'bcrypt'] as const;
 const timezoneSupported = ['UTC', 'America/Sao_Paulo', 'Europa/Rome'] as const;
 const localeSupported = ['pt-BR', 'en-US', 'it-IT'] as const;
+export const identityTypeSupported = ['uuidv4', 'uuidv7', 'nanoid'] as const;
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'stage', 'production']).default('development'),
@@ -26,6 +27,7 @@ const envSchema = z.object({
     DATABASE_PASSWORD: z.string().min(10).max(100).refine((value) => {
         return passwordStrength(value)
     }, { error: 'A senha do banco não atende os requisitos de segurança' }).optional(),
+    DATABASE_ID_DEFAULT: z.enum(identityTypeSupported, { error: `Defina um padrão de id para os registros do banco contindos nesta lista ${identityTypeSupported.join(',')}` }).default("uuidv7"),
 
     // Contatos de administradores
     EMAIL_TO_CONTACT: z.email(),
@@ -54,7 +56,14 @@ const envSchema = z.object({
         return val
     }).default(4),
     HASHER_TIME_COST: z.coerce.number().int().min(3, { error: 'Para o padrão de segurança o time cost deve no mínimo ser 3' }).default(3),
-    HASHER_MEMORY_COST: z.coerce.number().int().min(65536, 'O custo de processamento deve ser de no mínimo 64MB (ou seja 65536KiB)').default(65536)
+    HASHER_MEMORY_COST: z.coerce.number().int().min(65536, 'O custo de processamento deve ser de no mínimo 64MB (ou seja 65536KiB)').default(65536),
+
+    // Definição identificadores da aplicação
+    IDENTIFIER_PATTERN: z.enum(identityTypeSupported, { error: `Defina um tipode identificador suportado entre estes ${identityTypeSupported.join(', ')}` }).default("nanoid"),
+    IDENTIFIER_NANOID_ALPHABET: z.string().min(32, { error: 'alfabetos menores de 32 são inseguros' }).max(64, { error: 'alfabetos com mais de 64 caracteres fogem do URL-safe' }).refine((val) => {
+        return new Set(val).size === val.length
+    }, { error: 'O alfabeto não pode conter caracteres iguas, duplicatas reduzem a entropia' }).refine(val => /^[A-Za-z0-9\-_]+$/.test(val), { error: 'O alfabeto deve conter apenas caracteres URL safe' }).default("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"),
+    IDENTIFIER_NANOID_SIZE: z.coerce.number().int().min(16, { error: 'Mínimo 16 chars para entropia adequada' }).max(32, { error: 'Acima de 32 chars o ganho entrópico é desnecessário para IDs de aplicação' }).default(21)
 })
 
 
---
+++ src/core/identity/providers/NanoId.service.identity.ts
@@ -0,0 +1,41 @@
+import { StringWithLegthGen, type StringWithLegth } from "@Types/primitives.type.js";
+import BaseIdentityGenerator from "../contracts/IIdentyti.contract.js";
+import { env } from "@/configs/env.js";
+import { randomBytes } from "node:crypto";
+
+export default class NanoIdProvider extends BaseIdentityGenerator {
+    protected serviceName: string = 'NanoIdProvider';
+
+    private static readonly ALPHABET = env.IDENTIFIER_NANOID_ALPHABET;
+    private static readonly BITMASK = (2 << (Math.log(env.IDENTIFIER_NANOID_ALPHABET.length - 1) / Math.LN2 | 0)) - 1;
+    private static readonly DEFAULT_SIZE: number = env.IDENTIFIER_NANOID_SIZE // seguro para nano ID
+
+    protected generateLogic(): string {
+        const bytes = randomBytes(NanoIdProvider.DEFAULT_SIZE);
+        let id: string = '';
+
+        for (let i = 0; i < NanoIdProvider.DEFAULT_SIZE; i++) {
+            const index = bytes[i]! & NanoIdProvider.BITMASK;
+            let char = NanoIdProvider.ALPHABET[index];
+            if (char) {
+                id += char;
+                continue;
+            }
+            --i
+        }
+
+        return id;
+    }
+
+    protected generateValidation(id: string): boolean {
+
+        if (typeof id !== 'string' || id.length !== NanoIdProvider.DEFAULT_SIZE) {
+            this.identityLogger.warn({ serviceName: this.serviceName, length: id?.length, type: typeof id }, 'Tentativa de validação de NanoId com estrutura corrompida');
+            return false;
+        }
+
+        const pattern = new RegExp(`^[${NanoIdProvider.ALPHABET}]{${NanoIdProvider.DEFAULT_SIZE}}$`)
+        return pattern.test(id);
+    }
+
+}
