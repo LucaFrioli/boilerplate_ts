763824ec40250c763e98c9e01e89821ca5f3bf6e
LucaFrioli <lucafrioli7@gmail.com>
2026-03-06 18:09:05 -0300

    chore: tipando e reformulando a interface de usuário para poder escalar de forma segura

--- src/resources/User/User.interface.ts
+++ src/resources/User/User.interface.ts
@@ -1,19 +1,23 @@
-import type { UUID } from "node:crypto";
+import type { DatabaseID, AppID, HashedString } from '@Types'
 
-export default interface UserI{
-   readonly id?: UUID;
+export default interface UserI {
+   readonly id: DatabaseID;
+   readonly publicId: AppID;
    username: string;
    email: string;
-   password: string;
-   cpf?: string;
+   password: HashedString;
+   cpf: string;
+   active: boolean
+
    //--- Aqqui podemos ver campos que podem ser utilizados em relações 1:1 como o usuário é o pilar da maioria das aplicações e é com base nele que os acessos são concedidos, optei por deixar no boilerplate relação 1:1 nas relações já que cobre 90% dos usos recorrentes em aplicações mais padrões
    //####
-   stripeId?: string; // este campo está aqui para lembrar de sua existência incrmento para próxima versão do boilerplate
-   walletId?: string; // este campo será implementado em uma versão 3.0 ou 4.0
-   profileId?: UUID;
-   //---
-   createdAt?: Date;
-   updatedAt?: Date | null;
-   deletedAt?: Date | null;
+   stripeId: string | null; 
+   walletId: string | null; 
+   profileId: DatabaseID;
+   
+   // --- Auditoria
+   readonly createdAt: Date;
+   updatedAt: Date | null;
+   deletedAt: Date | null;
 }
 
