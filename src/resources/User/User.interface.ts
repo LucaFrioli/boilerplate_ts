7d3a1b3bc8e01a3295d52982de71e1a96cf9f221
LucaFrioli <lucafrioli7@gmail.com>
2026-03-08 17:19:10 -0300

    refactor: user interface com tipagem mais forte

--- src/resources/User/User.interface.ts
+++ src/resources/User/User.interface.ts
@@ -3,11 +3,11 @@ import type { DatabaseID, AppID, HashedString } from '@Types'
 export default interface UserI {
    readonly id: DatabaseID;
    readonly publicId: AppID;
+   active: boolean
    username: string;
    email: string;
-   password: HashedString;
+   passwordHash: HashedString;
    cpf: string;
-   active: boolean
 
    //--- Aqqui podemos ver campos que podem ser utilizados em relações 1:1 como o usuário é o pilar da maioria das aplicações e é com base nele que os acessos são concedidos, optei por deixar no boilerplate relação 1:1 nas relações já que cobre 90% dos usos recorrentes em aplicações mais padrões
    //####
