c5f589af8e4897b53ba29624cbf241ed5830b916
LucaFrioli <lucafrioli7@gmail.com>
2026-03-08 17:59:00 -0300

    chore: adicionando tipagem de garantia de imutabilidade para objetos aninhados

---
+++ src/shared/types/static.types.ts
@@ -0,0 +1,7 @@
+/**
+ * DeepDeepReadonly
+ * Tipagem que garante que objetos não sejma mutaveis independente de serem aninhados
+*/
+export type DeepReadonly<T> = {
+    readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P];
+};
