68ab147d97003dfe7749f227cee7f2028955fb9e
LucaFrioli <lucafrioli7@gmail.com>
2026-03-08 20:12:24 -0300

    chore: adicionando os principais métodos da entidade usuário

--- src/resources/User/User.ts
+++ src/resources/User/User.ts
@@ -1,11 +1,31 @@
 import { BaseEntity } from "@Contracts/Entity.contract.js";
-import type UserI from "./User.interface.js";
+import type { UserI, PublicUserI } from "./User.interface.js";
+import baseUserSchema from "./User.validation.js";
+import type { DeepReadonly } from "@/shared/types/static.types.js";
 
 
-export class User extends BaseEntity<UserI> {
+export class User extends BaseEntity<UserI, PublicUserI> {
+    protected entityName: string = 'User';
 
     protected validate(data: unknown): UserI {
-        throw new Error("Method validate on User class not implemented.");
+
+        const user = baseUserSchema.safeParse(data)
+        if (!user.success) {
+            const messages = user.error.issues.map(issue => issue.message)
+            this.handlingUserError('warn', this.entityName, { rawrErrors: user.error, messages: messages }, 'Erro ao tentar validar o usuário');
+        }
+
+        return user.data;
+    }
+
+    public toPublicDTO(): DeepReadonly<PublicUserI> {
+        return {
+            id: this.props.publicId,
+            username: this.props.username,
+            email: this.props.email,
+            active: this.props.active,
+            hasBillingProfile: this.props.stripeId !== null
+        }
     }
 
 }
