76cca0bc8c99875ea30feb70210ccd26bd19ec8d
LucaFrioli <lucafrioli7@gmail.com>
2026-05-26 20:37:57 -0300

    fix: adicionando máscara para logger do m´etodo changeUsername

--- src/resources/User/User.ts
+++ src/resources/User/User.ts
@@ -233,7 +233,10 @@ export class User extends BaseEntity<UserI, PublicUserI> implements UserMethods 
 				'warn',
 				{
 					typeofNewUseername: typeof newUsername,
-					rawValue: newUsername,
+					rawValue: maskPII(newUsername, this.entityLogger.child({
+						specificEntity: this.entityName,
+						classCalledMethodFrom: 'changeUsername',
+					})),
 					errorsCatched: z.treeifyError(validatedUsername.error),
 				},
 				'tentativa de trocar username por um username inválido',
