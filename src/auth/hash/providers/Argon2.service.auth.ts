e187a3a0e915da27c78f690cfeb9ebbda3ed91c1
LucaFrioli <lucafrioli7@gmail.com>
2026-07-29 06:40:15 +0200

    refact: add crypto deterministic hash to validate payload

--- src/auth/hash/providers/Argon2.service.auth.ts
+++ src/auth/hash/providers/Argon2.service.auth.ts
@@ -1,31 +1,37 @@
-import { BaseHasher } from '@Auth/hash/contracts/IHasher.contract.js';
+import { BaseHasher, type EnvForHasherProvider } from '@Auth/hash/contracts/IHasher.contract.js';
 import { argon2id, hash, verify, type Options as ArgonOptions } from 'argon2';
-import { env } from '@Configs/env.js';
 import { randomBytes } from 'node:crypto';
 import { regexValidationToHasherProvidersSupported } from '@Configs/Constants';
+import { DeterministicHash } from '@/core/cryptography/deterministicHash/DeterministicHash.factory.crypto.js';
 
 export default class Argon2Provider extends BaseHasher {
 	protected get ServiceName(): string {
 		return 'Argon2Provider';
 	}
 
-	private argonConfigs: ArgonOptions = {
-		type: argon2id,
+	private async genArgonConfigs(): Promise<ArgonOptions> {
+		const validEnv: EnvForHasherProvider = await this.validateEnvValues();
 
-		memoryCost: env.HASHER_MEMORY_COST,
-		timeCost: env.HASHER_TIME_COST,
-		parallelism: env.HASHER_PARALLELISM,
-
-		secret: Buffer.from(env.HASHER_SECURITY_PEPPER),
-		hashLength: env.HASHER_LENGTH,
-
-		salt: randomBytes(env.HASHER_SALT_LENGTH),
-	};
+		return ({
+			type: argon2id,
+			memoryCost: validEnv.HASHER_MEMORY_COST,
+			timeCost: validEnv.HASHER_TIME_COST,
+			parallelism: validEnv.HASHER_PARALLELISM,
+			secret: Buffer.from(validEnv.HASHER_SECURITY_PEPPER),
+			hashLength: validEnv.HASHER_LENGTH,
+			salt: randomBytes(validEnv.HASHER_SALT_LENGTH)
+		})
+	}
 
 	protected async executeHash(payload: string): Promise<string> {
-		const result = await hash(payload, {
-			...this.argonConfigs,
-			salt: randomBytes(env.HASHER_SALT_LENGTH),
+		const configs = await this.genArgonConfigs();
+		const { HASHER_SALT_LENGTH, HASHER_SECURITY_PEPPER } = (await this.validateEnvValues());
+
+		const securePayload = await DeterministicHash.hash(payload, HASHER_SECURITY_PEPPER)
+
+		const result = await hash(securePayload, {
+			...configs,
+			salt: randomBytes(HASHER_SALT_LENGTH),
 		});
 		return result;
 	}
@@ -35,8 +41,10 @@ export default class Argon2Provider extends BaseHasher {
 			return false;
 		}
 
+		const { secret } = await this.genArgonConfigs();
+
 		return await verify(hashedString, payload, {
-			secret: this.argonConfigs.secret,
+			secret: secret,
 		});
 	}
 
